55297c5009b9e760fd2af1e4fd0afee31c7673d6
Eivind Strand Harboe <eiha0403@gmail.com>
2022-02-17 11:38:16 +0100

    Added: BaseErrorMessage

---
+++ src/components/BaseErrorMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <p aria-live="assertive" :id="id"><slot /></p>
+</template>
+
+<script>
+export default {
+  name: "BaseErrorMessage",
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
