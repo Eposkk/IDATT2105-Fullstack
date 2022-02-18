1c7c3c5f7a77239e0d52c0deef72a167b922fd82
Eivind Strand Harboe <eiha0403@gmail.com>
2022-02-18 14:18:26 +0100

    Added: State with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,43 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: "",
+    Email: "",
+    submissions: {},
+    submission: [],
+  },
+  mutations: {
+    ADD_SUBMISSION(state, submission) {
+      state.submissions.push(submission);
+    },
+    SET_SUBMISSION(state, submission) {
+      state.submission = submission;
+    },
+    SET_USER(state, user) {
+      state.user = user;
+    },
+    SET_EMAIL(state, email) {
+      state.email = email;
+    },
+  },
+  actions: {
+    submitSubmission({ commit }, submission) {
+      console.log("Sent subbmissions" + submission.id);
+      commit("ADD_SUBMISSION", submission);
+      commit("SET_USER", submission.name);
+      commit("SET_EMAIL", submission.email);
+    },
+    getSubmission({ commit, state }, id) {
+      const existing = state.submissions.find(
+        (submission) => submission.id === id
+      );
+      if (existing) {
+        commit("SET_SUBMISSION", existing);
+      } else {
+        console.log("Error submission with id" + existing.id + " not found");
+      }
+    },
+  },
   modules: {},
 });
