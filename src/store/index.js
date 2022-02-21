beda0d1e63f1831760c603999c0ea892c240c16c
Eivind Strand Harboe <eiha0403@gmail.com>
2022-02-21 17:26:57 +0100

    Added: state management for users

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore } from "vuex";
+import FeedbackService from "@/features/FeedbackService";
 
 export default createStore({
   state: {
     user: "",
     email: "",
     flashMessage: "",
+    loginStatus: "",
     submissions: {},
     submission: [],
+    users: [],
+    currentUser: "",
   },
   mutations: {
     ADD_SUBMISSION(state, submission) {
@@ -23,6 +27,18 @@ export default createStore({
     },
     SET_FLASHMESSAGE(state, flashMessage) {
       state.flashMessage = flashMessage;
+    },
+    SET_LOGINSTATUS(state, loginStatus) {
+      state.loginStatus = loginStatus;
+    },
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    SET_CURRENT_USER(state, currentUser) {
+      state.currentUser = currentUser;
+    },
+    ADD_USER(state, user) {
+      state.users.push(user);
     },
   },
   actions: {
@@ -41,6 +57,34 @@ export default createStore({
       } else {
         console.log("Error submission with id" + existing.id + " not found");
       }
+    },
+    fetchUsers({ commit, state }) {
+      FeedbackService.getUsers()
+        .then((response) => {
+          commit("SET_USERS", response.data);
+          console.log(state.users.length);
+        })
+        .catch((error) => {
+          console.log("Error fetching users" + error);
+        });
+    },
+    fetchUserName({ commit, state }, username) {
+      const user = state.users.find((user) => user.username === username);
+      if (user) {
+        commit("SET_CURRENT_USER", user);
+      } else {
+        console.log("Error fetching user");
+      }
+    },
+    postNewUser({ commit }, user) {
+      commit("ADD_USER", user);
+      FeedbackService.postUser(user)
+        .then(() => {
+          console.log("Sent successfully");
+        })
+        .catch((error) => {
+          console.log("Error sending user " + error);
+        });
     },
   },
   modules: {},
