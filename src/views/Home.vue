6b584853b202eb4f097bf85b6bc1c34544553d02
Eivind Strand Harboe <eiha0403@gmail.com>
2022-02-23 15:57:02 +0100

    Changed: Home is now a home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
-  <Calculator></Calculator>
+  <div>
+    <h1 v-if="!name">Hi, please login!</h1>
+    <div v-if="name">
+      <h1>Welcome {{ name }}</h1>
+      <router-link class="routerLink" to="/calculator">Calculator</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Calculator from "../components/Calculator";
-
+import store from "@/store";
 export default {
   name: "Home",
-  components: {
-    Calculator,
+  setup() {
+    const name = store.state.currentUser.username;
+    return {
+      name,
+    };
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
